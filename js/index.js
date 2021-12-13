2949da73075f33cbff6bb2924dfe46c3ff12f6b3
MScheltjens <mathias.scheltjens@gmail.com>
2021-12-13 15:03:36 +0100

    added js script

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*
+We maken een constant variabele (const = nietmeer wijzigbaar)
+deze bevat 1 array met 3 objecten.
+elk object bevat 3 key-value pairs
+*/
+
+const todos= [
+    {
+        id: "123456",
+        todo: "Gras afrijden",
+        checked: true
+    },
+    {
+        id: "8765",
+        todo: "Tv kijken",
+        checked: false
+    },
+    {
+        id: "987654",
+        todo: "Auto poetsen",
+        checked: true
+    },
+]
